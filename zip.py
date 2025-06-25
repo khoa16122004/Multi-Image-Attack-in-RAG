331c4bc6a5ebd51464a2bbc741cc9e3225b0c204
khoa16122004 <khoatr.work@gmail.com>
2025-06-25 09:19:18 +0700

    update

---
+++ zip.py
@@ -0,0 +1,14 @@
+import zipfile
+import os
+
+def zip_folder(folder_path, output_zip_path):
+    with zipfile.ZipFile(output_zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                # Ghi vào zip với đường dẫn tương đối
+                arcname = os.path.relpath(file_path, start=folder_path)
+                zipf.write(file_path, arcname=arcname)
+
+# Ví dụ sử dụng
+zip_folder('attack_result', 'attack_result.zip')
