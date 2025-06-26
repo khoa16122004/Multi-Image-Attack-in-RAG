b5b9e42f3b8ddc98cfa69488409be1df25fe4df7
khoa16122004 <khoatr.work@gmail.com>
2025-06-26 14:28:45 +0700

    Update script_eval.py

--- script_eval.py
+++ script_eval.py
@@ -2,6 +2,9 @@ import pickle
 import numpy as np
 import json
 from util import DataLoader, arkiv_proccess, greedy_selection
+from tqdm import tqdm
+
+# param
 n_k = 1
 retriever_name = "clip"
 reader_name = "llava-one"
@@ -9,6 +12,7 @@ std = 0.05
 sample_id = 0
 run_path = "run.txt"
 
+# data
 all_scores = []
 success_retri = 0
 
@@ -16,7 +20,7 @@ success_retri = 0
 with open(run_path, "r") as f:
     sample_ids = [int(line.strip()) for line in f]
     
-for sample_id in sample_ids:
+for sample_id in tqdm(sample_ids):
     # calculate the L_topi and D_topi
     scores_path = f"attack_result/{retriever_name}_{reader_name}_{std}/{sample_id}/scores_{n_k}.pkl"
     with open(scores_path, "rb") as f:
@@ -25,6 +29,7 @@ for sample_id in sample_ids:
 
         final_front_score = np.array(scores[-1])
         selected_scores, success_retri = greedy_selection(final_front_score)
+        print(selected_scores)
         all_scores.append(selected_scores)
 
         if success_retri == True:
