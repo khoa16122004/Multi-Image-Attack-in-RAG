3d25c2bfebc8bf8f16bbf5cf4fa888559d82ebc1
khoa16122004 <khoatr.work@gmail.com>
2025-06-26 14:17:11 +0700

    update

--- reader.py
+++ reader.py
@@ -20,6 +20,8 @@ class Reader(torch.nn.Module):
                 pretrained="llava-next-interleave-qwen-7b",
                 model_name="llava_qwen",
             )
+            self.image_token = "<image>"
+            
         elif model_name == "llava-one":
             from lvlm_models.llava_ import LLava
             self.instruction = "Answer the given question based only on the visual content of the images. Do not guess or use outside knowledge. Keep the answer short."
@@ -27,6 +29,25 @@ class Reader(torch.nn.Module):
                 pretrained="llava-onevision-qwen2-7b-ov",
                 model_name="llava_qwen",
             )
+            self.image_token = "<image>"
+
+        elif model_name == "deepseek-vl":
+            from lvlm_models.deepseek_ import DeepSeek
+            self.instruction = "Answer the given question based only on the visual content of the images. Do not guess or use outside knowledge. Keep the answer short."
+            self.model = DeepSeek(
+                pretrained="DeepSeek-VL-7B-chat",
+            )
+            self.image_token = "<image_placeholder>"
+
+            
+        elif model_name == "mantis":
+            from lvlm_models.mantis_ import Mantis
+            self.instruction = "Answer the given question based only on the visual content of the images. Do not guess or use outside knowledge. Keep the answer short."
+            self.model = Mantis(
+                pretrained="Mantis-llava-7b",
+            )
+            self.image_token = "<image>"
+
             
     def init_data(self, golden_answer):
         self.answer = golden_answer
@@ -40,7 +61,7 @@ class Reader(torch.nn.Module):
           
     @torch.no_grad()
     def image_to_text(self, qs, img_files):
-        prompt = f"{self.instruction}\n question: {qs}\n images: " + "<image>" * len(img_files)
+        prompt = f"{self.instruction}\n question: {qs}\n images: " + self.image_token * len(img_files)
         text_output = self.model(prompt, img_files)  # string output
         return text_output
     
---
+++ script_eval.py
@@ -0,0 +1,22 @@
+import pickle
+import numpy as np
+import json
+from util import DataLoader, arkiv_proccess, greedy_selection
+n_k = 1
+retriever_name = "clip"
+reader_name = "llava-one"
+std = 0.05
+
+# calculate the L_topi and D_topi
+
+scores_path = "attack_result/{retriever_name}_{reader_name}_{std}/{sample_id}/scores_{n_k}.pkl"
+with open(scores_path, "rb") as f:
+    scores = pickle.load(f)
+    scores = arkiv_proccess(scores)
+
+    final_front_score = scores[-1]
+    selected_idx = greedy_selection(final_front_score)
+    selected_score = final_front_score[selected_idx]
+    print(selected_score)
+    # greedy selection
+    
