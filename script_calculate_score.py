d09a43663159ef043ea71b28ab635ec5ab2206f3
khoa16122004 <khoatr.work@gmail.com>
2025-06-27 14:36:06 +0700

    Update script_calculate_score.py

--- script_calculate_score.py
+++ script_calculate_score.py
@@ -24,9 +24,6 @@ def main(args):
         score = parse_score(score_response)
 
         if score is not None:
-            # print(score)
-            if score == 0.5:
-                score = 1
             total_score += score
             count += 1
 
