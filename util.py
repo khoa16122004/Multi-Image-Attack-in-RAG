f18dc27c40f8d31c3ff01679e7a113f7b33384c5
khoa16122004 <khoatr.work@gmail.com>
2025-07-01 13:03:17 +0700

    Update util.py

--- util.py
+++ util.py
@@ -8,7 +8,7 @@ import pickle
 from reader import Reader
 from retriever import Retriever
 from tqdm import tqdm
-from llm_service import LlamaService, GPTService
+from llm_service import LlamaService
 import re
 
 def dominate(a, b):
