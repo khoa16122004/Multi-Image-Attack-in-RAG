b7a8244a2bccda22833612a09a76755a94c72c08
khoa16122004 <khoatr.work@gmail.com>
2025-07-01 13:44:30 +0700

    Update mantis_.py

--- lvlm_models/mantis_.py
+++ lvlm_models/mantis_.py
@@ -25,7 +25,9 @@ class Mantis:
             self.processor,
             max_new_tokens=100,
             num_return_sequences=num_return_sequences,
-            eos_token_id=self.processor.tokenizer.eos_token_id,  # <-- thêm dòng này
+            eos_token_id=self.processor.tokenizer.eos_token_id, 
+            pad_token_id=self.processor.tokenizer.pad_token_id,
+
         )
         return [response]
 
