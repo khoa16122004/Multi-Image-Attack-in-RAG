3ea598708156f466674cf9ac644147ea44b13c0f
khoa16122004 <khoatr.work@gmail.com>
2025-07-07 10:55:58 +0700

    Update deepseekvl2.py

--- lvlm_models/deepseekvl2.py
+++ lvlm_models/deepseekvl2.py
@@ -53,7 +53,6 @@ class DeepSeekVL2:
             {"role": "<|Assistant|>", "content": answer},
         ]
 
-        # Prepare inputs with DeepSeek processor
         prepare = self.vl_chat_proccessor(
             conversations=conversation,
             images=img_files,
@@ -64,7 +63,6 @@ class DeepSeekVL2:
         input_ids = prepare.input_ids
         attention_mask = prepare.attention_mask
 
-        # Chạy mô hình để lấy logits
         with torch.no_grad():
             outputs = self.vl_gpt(
                 input_ids=input_ids,
@@ -73,19 +71,15 @@ class DeepSeekVL2:
                 use_cache=True,
             )
 
-        # Tính tổng log-prob của phần trả lời
-        logits = outputs.logits[:, :-1]  # shift right để khớp với label
-        labels = input_ids[:, 1:]        # label là token kế tiếp
+        logits = outputs.logits[:, :-1] 
+        labels = input_ids[:, 1:]       
 
-        # Lấy xác suất (log_softmax) và mask vị trí cần tính (chỉ phần trả lời)
         log_probs = torch.nn.functional.log_softmax(logits, dim=-1)
         log_probs_answer = log_probs.gather(2, labels.unsqueeze(-1)).squeeze(-1)
 
-        # Tìm vị trí bắt đầu của câu trả lời
         answer_start = (input_ids[0] == self.tokenizer.convert_tokens_to_ids("<|Assistant|>")).nonzero(as_tuple=True)[0][0] + 1
         log_probs_answer_part = log_probs_answer[0, answer_start:]
 
-        # Tính tổng log-prob
         total_log_prob = log_probs_answer_part.sum().item()
 
         return math.exp(total_log_prob)  # = p(answer | question, imgs)
@@ -97,6 +91,7 @@ if __name__ == "__main__":
 
     
     lvlm = DeepSeekVL2("deepseek-vl2-tiny")
+    print(lvlm(question, img_files))
     prob_1 = lvlm.compute_log_prob(question, img_files, "The shape of the nostrils on the bill of the Russet-naped Wood-Rail (Aramides albiventris) is not clearly visible in the provided images. However, based on the scientific name and the general appearance of the bird, it is likely that the nostrils are located on the upper mandible, which is the upper part of the beak. The upper mandible of the Russet-naped Wood-Rail is typically long and slender, with a slightly curved shape. The nostrils are usually located near the tip of the upper mandible, and they are typically small and oval-shaped.")
     print(prob_1)
     prob_2 = lvlm.compute_log_prob(question, img_files, "a bird")
