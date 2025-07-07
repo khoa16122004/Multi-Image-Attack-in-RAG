bee9551e0498754e6c581cc464e7ba00ceff27af
khoa16122004 <khoatr.work@gmail.com>
2025-07-07 13:55:01 +0700

    Create visualize_l_d_topij.py

---
+++ visualization/visualize_l_d_topij.py
@@ -0,0 +1,43 @@
+from util import *
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+model = [
+    "llava-one",
+    "llava-next",
+    "deepseek-vl2-tiny",
+    "qwen-vl",
+    "itern-vl"
+]
+
+color = [
+    'blue',
+    'red'
+]
+
+n_k = 1
+attack_result_dir = r"/data/elo/khoatn/VisualRAG/Multi-Image-Attack-in-RAG/attack_result_usingquestion=1"
+std="0.05"
+for model_name in model:
+    model_result_dir = os.path.join(attack_result_dir, f"{model_name}_{std}", )
+    avg_score_0 = []
+    avg_score_1 = []
+    for sample_id in lines:
+        path = os.path.join(model_result_dir, str(sample_id), f"scores_{n_k}.pkl")
+        with open(path, "rb") as f:
+            scores = pickle.load(f)
+            scores = arkiv_proccess(scores)
+            print(scores.shape)
+            raise
+            min_score_0 = np.min(final_front_score[:, 0])
+            min_score_1 = np.min(final_front_score[:, 1])
+            avg_score_0.append(min_score_0)
+            avg_score_1.append(min_score_1)
+
+    # calculate the
+
+
+
+
