fecba8dbaad2b3a0c4215859d776d44eebade3b8
khoa16122004 <khoatr.work@gmail.com>
2025-07-14 10:53:18 +0700

    update

--- run_all__llava_env_nk_5.sh
+++ run_all__llava_env_nk_5.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# Deepseekvl2-tiny
-CUDA_VISIBLE_DEVICES=5 python main.py \
-    --sample_path run.txt \
-    --result_clean_dir result_usingquery=0_clip \
-    --reader_name deepseek-vl2-tiny \
-    --retriever_name clip \
-    --pop_size 20  \
-    --F 0.9 \
-    --n_k 4 \
-    --max_iter 100 \
-    --std 0.05 \
-    --using_question 1
-
-
-# llava-one
 CUDA_VISIBLE_DEVICES=5 python main.py \
     --sample_path run.txt \
     --result_clean_dir result_usingquery=0_clip \
@@ -22,13 +7,12 @@ CUDA_VISIBLE_DEVICES=5 python main.py \
     --retriever_name clip \
     --pop_size 20  \
     --F 0.9 \
-    --n_k 4 \
+    --n_k 5 \
     --max_iter 100 \
     --std 0.05 \
     --using_question 1
 
 
-# llava-next
 CUDA_VISIBLE_DEVICES=5 python main.py \
     --sample_path run.txt \
     --result_clean_dir result_usingquery=0_clip \
@@ -36,7 +20,20 @@ CUDA_VISIBLE_DEVICES=5 python main.py \
     --retriever_name clip \
     --pop_size 20  \
     --F 0.9 \
-    --n_k 4 \
+    --n_k 5 \
+    --max_iter 100 \
+    --std 0.05 \
+    --using_question 1
+
+
+CUDA_VISIBLE_DEVICES=7 python main.py \
+    --sample_path run.txt \
+    --result_clean_dir result_usingquery=0_clip \
+    --reader_name qwenvl2.5 \
+    --retriever_name clip \
+    --pop_size 20  \
+    --F 0.9 \
+    --n_k 5 \
     --max_iter 100 \
     --std 0.05 \
     --using_question 1
