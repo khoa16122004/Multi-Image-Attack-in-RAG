edc11aab0a1ebc6b1cab2af0f39c1fc537a51515
khoa16122004 <khoatr.work@gmail.com>
2025-08-02 11:29:56 +0700

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,8 +1,9 @@
 from util import DataLoader
-from llm_service import GPTService
+from llm_service import GPTService, get_prompt_compare_answer, parse_score
 from reader import Reader
 import os
 import json
+
 
 max_topk = 5
 model_name = "llava-one"
@@ -13,20 +14,36 @@ with open(run_path, "r") as f:
     sample_ids = [int(line.strip()) for line in f]   
 llm = GPTService("gpt-4o")
 output_dir = "clean_result"
-# os.makedirs(output_dir, eexist_ok=True)
+os.makedirs(output_dir, exist_ok=True)
 loader = DataLoader(
     retri_dir=r"result_usingquery=0_clip",
 )
 # reader = Reader(reader_name="llava-one")
 
 attack_result_dir = f"attack_result_usingquestion=1/clip_{model_name}_0.05"
-
+model_dir = os.path.join(output_dir, f"clean_gt_{model_name}")
 for sample_id in sample_ids:
     for k in range(1, max_topk + 1):
+        top_k_path = os.path.join(model_dir, f"eval_results_top{k}.json")
         question, gt_answer, query, gt_basenames, retri_basenames, retri_imgs, sims = loader.take_retri_data(sample_id)
         with open(os.path.join(attack_result_dir, str(sample_id), f"answers_{k}.json"), "r") as f:
             data = json.load(f)
             golden_answer = data['golden_answer']
-        print(golden_answer)
-        print(gt_answer)
-        break
+        system_prompt, user_prompt = get_prompt_compare_answer(gt_answer=gt_answer, model_answer=golden_answer, question=question)
+        score_response = llm.text_to_text(system_prompt=system_prompt, prompt=user_prompt).strip()
+        end_to_end_score = parse_score(score_response)        
+
+        output_data = {
+            "gt_answer": gt_answer,
+            "golden_answer": golden_answer,
+            "score_response": score_response,
+            "parse_score": end_to_end_score,
+        }
+        
+        # Save the output data to a JSON file
+        output_file_path = os.path.join(output_dir, f"sample_{sample_id}_top{k}.json")
+        with open(output_file_path, "w") as output_file:
+            json.dump(output_data, output_file, indent=4)
+        
+        raise
+
